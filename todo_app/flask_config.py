6592e9121fb0fa083db95b582377f987adbd2a8b
Steven Hay <contact@stevenhay.dev>
2021-07-16 15:04:14 +0100

    Remove flask Config

--- todo_app/flask_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    """Base configuration variables."""
-    MONGO_URL = 'cluster0.brtrv.mongodb.net'
-    MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
-    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-    MONGO_DATABASE = 'todoapp'
